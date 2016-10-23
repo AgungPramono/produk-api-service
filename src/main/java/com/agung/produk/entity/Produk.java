a0b42c348436a6754a9d572190525313652498e9
agungpramono <agungpermadi13@gmail.com>
2016-10-23 18:35:26 +0700

    menambah class entity

---
+++ src/main/java/com/agung/produk/entity/Produk.java
@@ -0,0 +1,80 @@
+//
+// Produk.java
+// produk-api-service 
+//
+// Created by Agung Pramono on 23/10/2016 
+// Copyright (c) 2016 Java Development. All rights reserved.
+//
+
+package com.agung.produk.entity;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ */
+@Entity
+@Table(name = "m_produk")
+public class Produk {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    @NotNull @NotEmpty @Size(min = 3, max = 10)
+    @Column(nullable = false, unique = true)
+    private String code;
+    
+    @NotNull @NotEmpty @Size(min = 3, max = 10)
+    @Column(nullable = false)
+    private String name;
+    
+    @NotNull @Min(0)
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    
+    
+}
